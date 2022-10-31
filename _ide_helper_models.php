275962bfda82a23012d7a5649fede60c7367cb49
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-10-31 20:50:28 +0900

    chore _ide_helper_models

---
+++ _ide_helper_models.php
@@ -0,0 +1,72 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\AttendanceRecord
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $start_time
+ * @property \Illuminate\Support\Carbon|null $end_time
+ * @property \Illuminate\Support\Carbon $total_minutes
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord query()
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord whereEndTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord whereStartTime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord whereTotalMinutes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttendanceRecord whereUserId($value)
+ */
+	class AttendanceRecord extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\AttendanceRecord[] $attendanceRecord
+ * @property-read int|null $attendance_record_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
