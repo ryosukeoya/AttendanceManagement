ca8912a696163e423142fba0577fae332fb2cb05
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-11-01 18:07:07 +0900

    fix fetch fuction

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,9 +8,9 @@ Alpine.start()
 
 // TODO　場所変える
 window.onload = () => {
-    try {
-        async function getRecord() {
-            const response = await window
+    async function getRecord() {
+        try {
+            await window
                 .fetch('api_attendance_record', {
                     method: 'GET'
                 })
@@ -20,31 +20,34 @@ window.onload = () => {
                     }
                     return res.json()
                 })
+                .then((result) => {
+
+                    // TODO Refactor
+                    if (result.attendanceStatus == 1) {
+                        const workingStartMenu = document.getElementById('workingStart')
+
+                        workingStartMenu.classList.add('gray')
+                        workingStartMenu.classList.add('pointer_events_none')
+                        workingStartMenu.firstElementChild.href = 'javascript:void(0)'
+                    } else if (result.attendanceStatus == 2) {
+                        const workingStartMenu = document.getElementById('workingStart')
+                        const workingEndMenu = document.getElementById('workingEnd')
+
+                        workingStartMenu.classList.add('gray')
+                        workingStartMenu.classList.add('pointer_events_none')
+                        workingStartMenu.firstElementChild.href = 'javascript:void(0)'
+                        workingEndMenu.classList.add('gray')
+                        workingEndMenu.classList.add('pointer_events_none')
+                        workingEndMenu.firstElementChild.href = 'javascript:void(0)'
+                    }
+                    return result
+                })
                 .catch((error) => {
                     console.error(error)
                 })
-
-            // TODO Refactor
-            if (response.attendanceStatus == 1) {
-                const workingStartMenu = document.getElementById('workingStart')
-
-                workingStartMenu.classList.add('gray')
-                workingStartMenu.classList.add('pointer_events_none')
-                workingStartMenu.firstElementChild.href = 'javascript:void(0)'
-            } else if (response.reportedStatus == 2) {
-                const workingStartMenu = document.getElementById('workingStart')
-                const workingEndMenu = document.getElementById('workingEnd')
-
-                workingStartMenu.classList.add('gray')
-                workingStartMenu.classList.add('pointer_events_none')
-                workingStartMenu.firstElementChild.href = 'javascript:void(0)'
-                workingEndMenu.classList.add('gray')
-                workingEndMenu.classList.add('pointer_events_none')
-                workingEndMenu.firstElementChild.href = 'javascript:void(0)'
-            }
+        } catch (error) {
+            console.error(error)
         }
-        getRecord()
-    } catch (error) {
-        console.error(error)
     }
+    getRecord()
 }
