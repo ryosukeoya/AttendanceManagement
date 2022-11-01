c118ebde8a6d3b32923eebbbab2116360ac6471c
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-11-01 18:02:38 +0900

    fix update attendance record function

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,7 +1,7 @@
-@props(['title','href'])
+@props(['id','title','href'])
 
 {{-- TODO data → role? --}}
-<div data="menu-item" class="shadow-sm border-gray-200">
+<div id={{$id}} data="menu-item" class="shadow-sm border-gray-200">
     <a href={{$href}} class="block p-4 hover:bg-gray-50">
         {{$title}}
     </a>
