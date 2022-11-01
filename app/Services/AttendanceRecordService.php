94929013a167a94f795922bf889c4d74eeb01901
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-11-01 11:24:57 +0900

    refactor attendanceRecordService

--- app/Services/AttendanceRecordService.php
+++ app/Services/AttendanceRecordService.php
@@ -8,29 +8,40 @@ use Carbon\Carbon;
 class AttendanceRecordService
 {
     private const ATTENDANCE_STATUS = [
+        '未登録' => 0,
         '始業済み' => 1,
         '終業済み' => 2,
+        '不正登録' => 3,
     ];
 
     final public function getAttendanceStatus(User $user, Carbon $date): int
     {
-        $startedRecordCounts = $this->getStartedRecordCounts($user, $date);
-        $endedRecordCounts = $this->getEndedRecordCounts($user, $date);
+        $startedRecordCounts = $this->getTodayStartedRecordCounts($user, $date);
+        $endedRecordCounts = $this->getTodayEndedRecordCounts($user, $date);
 
+        $attendanceStatus = $this->assignAttendanceStatus($startedRecordCounts, $endedRecordCounts);
+        return $attendanceStatus;
+    }
+
+    final private function assignAttendanceStatus(int $startedRecordCounts, int $endedRecordCounts): int
+    {
         try {
-            if ($startedRecordCounts >= 1 && $endedRecordCounts >= 1) {
-                return self::ATTENDANCE_STATUS['終業済み'];
-            } elseif ($startedRecordCounts >= 1) {
+            if ($startedRecordCounts == 0 && $endedRecordCounts == 0) {
+                return self::ATTENDANCE_STATUS['未登録'];
+            } elseif ($startedRecordCounts >= 1 && $endedRecordCounts == 0) {
                 return self::ATTENDANCE_STATUS['始業済み'];
-            } elseif ($endedRecordCounts >= 1) {
-                throw new \RecordException();
+            } elseif ($startedRecordCounts == 0 && $endedRecordCounts >= 1) {
+                throw new \RecordException('未始業かつ終業済み登録');
+            } elseif ($startedRecordCounts >= 1 && $endedRecordCounts >= 1) {
+                return self::ATTENDANCE_STATUS['終業済み'];
             }
         } catch (\RecordException $e) {
-            \Log::error('ABNORMAL_RECORD : ', $e);
+            \Log::error('ABNORMAL_RECORD : ', $e->getMessage());
+            return self::ATTENDANCE_STATUS['不正登録'];
         }
     }
 
-    final private function getStartedRecordCounts(User $user, Carbon $date): int
+    final private function getTodayStartedRecordCounts(User $user, Carbon $date): int
     {
         $todayDateString = \DateConverter::getTodayString($date, 'YYYY-MM-DD');
         $todaysDateRegex = '%' . $todayDateString . '%';
@@ -45,13 +56,14 @@ class AttendanceRecordService
                 throw new \RecordException('today started record 1 than many');
             }
         } catch (\RecordException $e) {
-            \Log::error('STARTED_RECORD : ', $e->getMessage());
+            \Log::error('STARTED_RECORD  : ', $e->getMessage());
+            return $startedRecordCounts;
         }
 
         return $startedRecordCounts;
     }
 
-    final private function getEndedRecordCounts(User $user, Carbon $date): int
+    final private function getTodayEndedRecordCounts(User $user, Carbon $date): int
     {
         $todayDateString = \DateConverter::getTodayString($date, 'YYYY-MM-DD');
         $todaysDateRegex = '%' . $todayDateString . '%';
@@ -67,6 +79,7 @@ class AttendanceRecordService
             }
         } catch (\RecordException $e) {
             \Log::error('ENDED_RECORD : ', $e->getMessage());
+            return $endedRecordCounts;
         }
 
         return $endedRecordCounts;
