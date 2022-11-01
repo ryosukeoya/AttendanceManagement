39dcc0c9bf3e336cceccdac34e1fd5f5b0644d6a
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-11-01 18:03:10 +0900

    improve home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     <div class="mt-8 flex flex-col justify-end max-w-5xl m-auto bg-white">
         <x-menu-item id="workingStart" title="勤務開始" href="{{route('attendance_record.create')}}"></x-menu-item>
         {{-- TODO attendance_record --}}
-        <x-menu-item id="workingEnd" title="勤務終了" href="{{route('attendance_record.edit',['attendance_record' => 2])}}"></x-menu-item>
+        <x-menu-item id="workingEnd" title="勤務終了" href="{{route('attendance_record.edit',['attendance_record' => 1])}}"></x-menu-item>
         <x-menu-item id="workingHistory" title="勤務履歴" href="{{route('attendance_record.index')}}"></x-menu-item>
     </div>
 </x-app-layout>
