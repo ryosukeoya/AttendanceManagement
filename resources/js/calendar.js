2800aa6f3b3271ebf78a0ba17b2781e5a56cf3c2
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-12-14 21:30:50 +0900

    add calendar modal content

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,8 +1,11 @@
+'use strict'
+
 import { Calendar } from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
+import { getHours, getMinutes } from 'date-fns'
 
 /**
  * TODO プリント勤務
@@ -19,31 +22,60 @@ document.addEventListener('DOMContentLoaded', async function () {
 
             modalBackdrop.classList.remove('hidden')
             modalBackdrop.classList.add('block')
+
+            const start = document.getElementById('start')
+            const end = document.getElementById('end')
+            const total = document.getElementById('total')
+
+            const startHours = getHours(info.event._instance.range.start)
+            const startMinutes = getMinutes(info.event._instance.range.start)
+            start.textContent = `勤務開始時間 : ${startHours}時${startMinutes}分`
+
+            const endHours = getHours(info.event._instance.range.end)
+            const endMinutes = getMinutes(info.event._instance.range.end)
+            end.textContent = `勤務終了時間 : ${endHours}時${endMinutes}分`
+
+            // TODO Refactor
+            // TODO　日付跨ぎ
+            const totalMilliSeconds = info.event._instance.range.end - info.event._instance.range.start
+            console.log(totalMilliSeconds)
+            const totalSeconds = totalMilliSeconds / 1000
+            console.log(totalSeconds)
+            const totalHours = Math.floor(totalSeconds / 3600)
+            const totalMinutes = (totalSeconds % 3600) / 60
+
+            total.textContent = `合計勤務時間 : ${totalHours}時間${totalMinutes}分`
         }
     })
     calendar.render()
 
-    const result = await window
-        .fetch('api_calendar', { method: 'GET' })
-        .then((res) => {
-            return res.json()
-        })
-        .then((result) => {
-            return result
-        })
-        .catch((error) => {
-            console.error(error)
+    try {
+        const result = await window
+            .fetch('api_attendance_record/me/all', { method: 'GET' })
+            .then((res) => {
+                if (!res.ok) {
+                    console.error(res.status, 'server error')
+                }
+                return res.json()
+            })
+            .then((result) => {
+                return result
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+
+        const eventSources = result.calendarResources.map((resource) => {
+            return {
+                title: '勤務',
+                start: resource.start_time,
+                end: resource.end_time,
+                constraint: 'businessHours'
+            }
         })
 
-    const eventSources = []
-    result.calendarResources.forEach((resource) => {
-        eventSources.push({
-            title: '勤務',
-            start: resource.start_time,
-            end: resource.end_time,
-            constraint: 'businessHours'
-        })
-    })
-
-    calendar.addEventSource(eventSources)
+        calendar.addEventSource(eventSources)
+    } catch (error) {
+        console.error(error)
+    }
 })
