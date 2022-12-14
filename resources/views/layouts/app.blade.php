49153e66af8050dbb710add36702c5fb4245f9ed
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-12-14 16:49:25 +0900

    change from cdn to npm management for fullcalendar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,10 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    {{-- TDDO Refactor --}}
-    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css' rel='stylesheet' />
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js'></script>
-    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/interaction@5.11.3/main.global.min.js'></script>
-    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@5.11.3/main.global.min.js'></script>
     {{-- Refactor --}}
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/menuState.js', 'resources/js/calendar.js','resources/js/modal.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/menuState.js',
+    'resources/js/calendar.js','resources/js/modal.js'])
 </head>
 
 <body class="font-sans antialiased">
