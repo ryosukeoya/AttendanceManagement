9bd926865b4975e105377b0cf26cb5c82f9da1a5
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-12-15 13:38:36 +0900

    improve ui for modal.blade.php

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,6 +1,6 @@
 <div id="modalBackdrop"
     class="hidden fixed top-0 bg-gray-700 w-full h-full min-x-screen bg-opacity-50 cursor-pointer z-10">
-    <div id="modalContent" class="absolute bg-white inset-0 m-auto w-96 h-96 p-7 rounded-lg animate-appear">
+    <div id="modalContent" class="absolute bg-white inset-0 m-auto w-96 h-96 p-7 rounded-lg overflow-y-scroll animate-appear">
         <p id="start"></p>
         <p id="end"></p>
         <p id="total"></p>
