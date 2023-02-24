fd4c00d5c6a8b61e5e0207201e057635ed5cb833
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2023-02-24 09:33:33 +0900

    fix input modules of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import checker from 'vite-plugin-checker'
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/ts/app.ts'
-            ],
+            input: ['resources/css/app.css', 'resources/ts/app.ts'],
             refresh: true
         }),
         checker({ typescript: true })
